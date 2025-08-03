c7e919d40c99954bc77884a1b8dd325f87f9e027
Shinsan09 <S.yamamoto870909@gmail.com>
2025-08-03 04:14:17 +0000

    コメントさらに変更

--- hello_world/calculate.py
+++ hello_world/calculate.py
@@ -17,7 +17,7 @@ def main():
             except ValueError:
                 print("無効な入力です。数字を入力してください。")
 
-    # 平均を計算aaaccc
+    # 平均を計算aaacccdddd
     average = calculate_average(numbers)
 
     # 結果を表示
